0b8c85dd4ab359d9fe20e55cf7536b7f0ff07b7e
Shubankar <shubankar.s.06@gmail.com>
2025-08-08 02:34:51 +0530

    backend commit without login

--- Backend/routes_admin.py
+++ Backend/routes_admin.py
@@ -9,13 +9,3 @@ def get_all_hospitals():
     hospitals = Hospital.query.all()
     return jsonify([{"id": h.id, "hospital_id": h.hospital_id, "hospital_name": h.hospital_name} for h in hospitals])
 
-@admin_bp.route('/user-records/<user_id>', methods=['GET'])
-def get_user_records(user_id):
-    records = VaccinationRecord.query.filter_by(user_id=user_id).all()
-    return jsonify([{
-        "vaccine_type": r.vaccine_type,
-        "disease": r.disease,
-        "dose_number": r.dose_number,
-        "date_administered": r.date_administered,
-        "hospital_id": r.hospital_id
-    } for r in records])
